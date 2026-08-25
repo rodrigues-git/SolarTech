edb909bb4ee0a831503abf81575c426cb5d17ce3
Fábio Félix Rodrigues <fabiorod.it@gmail.com>
2026-08-25 15:50:42 -0300

    continuação do pronjeto

---
+++ src/js/faq.js
@@ -0,0 +1,17 @@
+const botoes = document.querySelectorAll('.botao_faq')
+
+botoes.forEach((botao) => {
+    botao.addEventListener("click", () => {
+
+        const resposta = botao.nextElementSibling
+
+        if(resposta.style.display == 'block'){
+             resposta.style.display = 'none'
+        }
+        else{
+            resposta.style.display = 'block'
+        }
+       
+    })
+})
+
--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,6 @@ function expandirMenu(){
     else{
         menu.style.display = 'block'
     }
-  
 }
 
 
