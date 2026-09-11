5b417d647fbb5ca8535662f39a512e77b60fc51c
Fábio Félix Rodrigues <fabiorod.it@gmail.com>
2026-09-11 20:27:41 -0300

    finalização da versão desktop

--- src/js/footer_mobile.js
+++ src/js/footer_mobile.js
@@ -3,6 +3,7 @@ const botoes_footer = document.querySelectorAll('#footer_1 button')
 botoes_footer.forEach((botao) => {
 
     botao.addEventListener('click', () => {
+        console.log('clicou')
         const ul = botao.nextElementSibling
 
         ul.classList.toggle('lista_footer')
